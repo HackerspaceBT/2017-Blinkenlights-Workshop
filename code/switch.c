aa86afa905a753cdb90df9d7294ad74a3d8bd03b
Valentin Ochs <a@0au.de>
2017-03-21 10:15:13 +0100

    Switch Beispiel Fixes

--- code/switch.c
+++ code/switch.c
@@ -1,17 +1,16 @@
 int const button_pin = 12, led_pin = 13;
 unsigned long button_time = 0;
 bool running = false;
-void setup() { pinMode(led_pin, OUTPUT); }
+void setup(){ pinMode(led_pin, OUTPUT);
+              pinMode(button_pin, INPUT_PULLUP); }
 void loop() {
-  if(digitalRead(button_pin) == HIGH) {
+  if(digitalRead(button_pin) == LOW) {
     running = true;
     button_time = millis();
   }
-  if(running) {
+  if(running)
     running = do_stuff(millis() - button_time);
-  }
 }
-
 bool do_stuff(unsigned long time_point) {
   if(time_point < 100) {
     digitalWrite(led_pin, HIGH);
